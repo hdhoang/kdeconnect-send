5b01d379c84fbf80b03bc6830a40f5265f27abec
Hoàng Đức Hiếu <hdhoang@zahe.me>
2015-01-14 13:13:37 +0700

    clone each id string instead of the whole array

--- src/main.rs
+++ src/main.rs
@@ -16,8 +16,8 @@ fn get_ids(c: &Connection) -> Result<Vec<String>, &'static str> {
     let items = m.get_items();
     match items[0] {
         MessageItem::Array(_, 0) => Err("No reachable devices"),
-        MessageItem::Array(ref a, _) => Ok(a.clone().into_iter().map(|e| match e {
-            MessageItem::Str(s) => s,
+        MessageItem::Array(ref a, _) => Ok(a.iter().map(|&ref e| match *e {
+            MessageItem::Str(ref s) => s.clone(),
             _ => unreachable!(),
         }).collect()),
         _ => unreachable!(),
