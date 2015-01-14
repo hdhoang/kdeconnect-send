dcc8634da3ba0c501f1cb51267fbc1432f6c39eb
Hoàng Đức Hiếu <hdhoang@zahe.me>
2015-01-14 15:55:42 +0700

    use new slice syntax

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ static PATH: &'static str = "/modules/kdeconnect";
 fn get_ids(c: &Connection) -> Result<Vec<String>, &'static str> {
     let mut m = Message::new_method_call(DEST
                                          , PATH
-                                         , [DEST, "daemon"].connect(".").as_slice()
+                                         , &[DEST, "daemon"].connect(".")[]
                                          , "devices").unwrap();
     m.append_items(&[MessageItem::Bool(true)]); // onlyReachable
     m = c.send_with_reply_and_block(m, 1000).unwrap();
@@ -26,16 +26,15 @@ fn get_ids(c: &Connection) -> Result<Vec<String>, &'static str> {
 
 fn share_url(c: &Connection, id: &String, url: &String) {
     let mut m = Message::new_method_call(DEST
-                                         , [PATH, "devices", id.as_slice(), "share"].connect("/").as_slice()
-                                         , [DEST, "device.share"].connect(".").as_slice()
+                                         , &[PATH, "devices", &id[], "share"].connect("/")[]
+                                         , &[DEST, "device.share"].connect(".")[]
                                          , "shareUrl").unwrap();
     m.append_items(&[MessageItem::Str(url.clone())]);
     c.send_with_reply_and_block(m, 1000).unwrap();
 }
 
 fn main() {
-    let mut urls = args();
-    urls.remove(0);
+    let urls = &args()[1..];
 
     let ref c = Connection::get_private(BusType::Session).unwrap();
     let ids = get_ids(c).unwrap();
